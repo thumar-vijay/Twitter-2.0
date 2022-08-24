83aa9a7e881c40073d5c37b3091b90d1ec621ad4
vijay-thumar <vijaythumar1020@gmail.com>
2022-08-24 13:22:40 +0530

    added take api respoce and add it to feed component

--- twitter2.0/pages/index.tsx
+++ twitter2.0/pages/index.tsx
@@ -3,8 +3,16 @@ import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweet'
 
-const Home: NextPage = () => {
+interface props{
+  tweets: Tweet[]
+}
+
+const Home = ({tweets}: props) => {
+  // console.log(tweets);
+  
   return (
     <div className='lg:max-w-6xl mx-auto max-h-screen overflow-hidden'>
       <Head>
@@ -16,7 +24,7 @@ const Home: NextPage = () => {
 
         <Sidebar/>
 
-        <Feed/>
+        <Feed tweets={tweets}/>
 
         <Widgets/>
         
@@ -29,10 +37,10 @@ const Home: NextPage = () => {
 export default Home
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  // const tweets = await fetchTweets();
+  const tweets = await fetchTweets();
   return{
     props:{
-
+      tweets  
     }
   }
 }
